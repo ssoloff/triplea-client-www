60f11096cbdf13ef65de02d6935609e1d6302034
Steven Soloff <steven.soloff@gmail.com>
2017-07-09 00:00:20 -0400

    Use stateless functional components where possible

--- src/client/components/App/App.jsx
+++ src/client/components/App/App.jsx
@@ -4,18 +4,16 @@ import Lobby from '../Lobby'
 import React from 'react'
 import styles from './App.css'
 
-export default class App extends React.Component {
-  render () {
-    return (
-      <Router>
-        <div className='container'>
-          <h1>TripleA</h1>
-          <p className={styles.subtitle}>The World&apos;s Foremost Open Source Grand Strategy Game</p>
+export default function App () {
+  return (
+    <Router>
+      <div className='container'>
+        <h1>TripleA</h1>
+        <p className={styles.subtitle}>The World&apos;s Foremost Open Source Grand Strategy Game</p>
 
-          <Route exact path='/' component={Home}/>
-          <Route path='/lobby' component={Lobby}/>
-        </div>
-      </Router>
-    )
-  }
+        <Route exact path='/' component={Home}/>
+        <Route path='/lobby' component={Lobby}/>
+      </div>
+    </Router>
+  )
 }
--- src/client/components/Lobby/Lobby.jsx
+++ src/client/components/Lobby/Lobby.jsx
@@ -2,18 +2,16 @@ import { Link } from 'react-router-dom'
 import React from 'react'
 import styles from './Lobby.css'
 
-export default class Lobby extends React.Component {
-  render () {
-    return (
-      <div>
-        <h2>Lobby</h2>
-        <p className={styles.notice}>
-          Lobby Rules: https://forums.triplea-game.org/topic/4<br/>
-          No talking politics in the lobby! Take your political comments to a private game!<br/>
-          Please ensure you are using the latest stable release of Triplea located here http://www.triplea-game.org/
-        </p>
-        <p>Return to <Link to='/'>main menu</Link></p>
-      </div>
-    )
-  }
+export default function Lobby () {
+  return (
+    <div>
+      <h2>Lobby</h2>
+      <p className={styles.notice}>
+        Lobby Rules: https://forums.triplea-game.org/topic/4<br/>
+        No talking politics in the lobby! Take your political comments to a private game!<br/>
+        Please ensure you are using the latest stable release of Triplea located here http://www.triplea-game.org/
+      </p>
+      <p>Return to <Link to='/'>main menu</Link></p>
+    </div>
+  )
 }
