8755c856cc23a657b4da67640f25c6e5060882db
Steven Soloff <steven.soloff@gmail.com>
2017-07-10 00:54:59 -0400

    Add support for Flow

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,17 @@
 import config from './config'
 import express from 'express'
 import path from 'path'
+import sum from './lib/sum'
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/api/sum', (req, res) => {
+  res.json({
+    sum: sum(parseInt(req.query.augend, 10), parseInt(req.query.addend, 10))
+  })
+})
 
 app.listen(config.port, () => {
   console.log(`The server is running at http://localhost:${config.port}/`) // eslint-disable-line no-console
--- src/server/lib/sum.js
+++ src/server/lib/sum.js
@@ -1,3 +1,5 @@
-export default function sum (augend, addend) {
+// @flow
+
+export default function sum (augend: number, addend: number): number {
   return augend + addend
 }
