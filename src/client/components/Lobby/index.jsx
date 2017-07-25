9f5248dce273fb5718a0215319c0dd69fdbf6d9d
Steven Soloff <steven.soloff@gmail.com>
2017-07-24 23:54:18 -0400

    Move lobby user list to its own column

--- src/client/components/Lobby/index.jsx
+++ src/client/components/Lobby/index.jsx
@@ -18,15 +18,21 @@ export default class Lobby extends React.Component {
 
   render () {
     return (
-      <div>
+      <div className='container'>
         <h2>Lobby</h2>
-        <p className={styles.notice}>
-          Lobby Rules: https://forums.triplea-game.org/topic/4<br/>
-          No talking politics in the lobby! Take your political comments to a private game!<br/>
-          Please ensure you are using the latest stable release of Triplea located here http://www.triplea-game.org/
-        </p>
-        <Users users={this.state.users} />
-        <p>Return to <Link to='/'>main menu</Link></p>
+        <div className='row'>
+          <div className='col-sm-9'>
+            <p className={styles.notice}>
+              Lobby Rules: https://forums.triplea-game.org/topic/4<br/>
+              No talking politics in the lobby! Take your political comments to a private game!<br/>
+              Please ensure you are using the latest stable release of Triplea located here http://www.triplea-game.org/
+            </p>
+            <p>Return to <Link to='/'>main menu</Link></p>
+          </div>
+          <div className='col-sm-3'>
+            <Users users={this.state.users} />
+          </div>
+        </div>
       </div>
     )
   }
